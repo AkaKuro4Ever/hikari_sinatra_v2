e5e97de9d2784173cf8db0b8e20cf9efa3cdc7ff
Deborah Seow <deborahseow9@gmail.com>
2018-04-28 04:52:14 +0000

    Added User Controller and began session basics

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :sessions_secret, "secret"
+  end
+
+  #The configure block above is a part of built-in settings that control whether features are enabled or not. In this case, we're enabling the sessions feature.
+
+  get '/login' do
+
+  end
+
+  post '/' do
+
+  end
+
+  get '/:username' do
+    @session = session
+    #Because we enabled sessions in our app, every controller action has access to the session hash.
+  end
+end
